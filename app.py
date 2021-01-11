91ec50a45e5a16a09f4be7d0521cd4c7f306dc96
barnez299 <barnez29@gmail.com>
2021-01-12 01:42:36 +0200

    display posts on homepage

--- app.py
+++ app.py
@@ -11,18 +11,35 @@ app.config['SECRET_KEY'] = 'thisisasecret'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost:5432/flaskblog2'
 # db = SQLAlchemy(app)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
 
 @app.route("/")
 @app.route("/home")
+
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 @app.route("/account")
-def contact():
+def account():
     return render_template('account.html', title='Account')
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -37,7 +54,8 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return render_template('home.html', title='Home')
+    return redirect(url_for('home'))
+
 
 
 if __name__ == '__main__':
