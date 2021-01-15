69abf185840b842d1a4aeab53321e9240c965825
barnez299 <barnez29@gmail.com>
2021-01-15 22:43:05 +0200

    initial blueprint folder structure

--- flaskblog2/__init__.py
+++ flaskblog2/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
 
 
@@ -20,8 +20,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'flaskybarnpy@gmail.com'
-app.config['MAIL_PASSWORD'] = 'P9517400'
+app.config['MAIL_USERNAME'] = '********'
+app.config['MAIL_PASSWORD'] = '******'
 mail = Mail(app)
 
 
